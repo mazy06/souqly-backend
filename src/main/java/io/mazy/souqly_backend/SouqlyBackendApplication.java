563132a5e49b7ecf40938bca3df6b043bcfa70a7
Mazy Toufik <mazytoufik@outlook.com>
2025-06-30 19:50:14 +0200

    Initial commit

---
+++ src/main/java/io/mazy/souqly_backend/SouqlyBackendApplication.java
@@ -0,0 +1,13 @@
+package io.mazy.souqly_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SouqlyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SouqlyBackendApplication.class, args);
+	}
+
+}
